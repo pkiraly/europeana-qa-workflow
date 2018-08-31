3eada632ed4d263262d55f6af5a40f1f8511195f
Peter Kiraly <peter.kiraly@gwdg.de>
2018-08-31 17:09:19 +0200

    Applying configuration.

--- dispatcher.sh
+++ dispatcher.sh
@@ -4,18 +4,18 @@
 
 echo $STATUS_SERVER_URL
 
-res=$(curl -s localhost:12345/next.php)
+status=$(curl -s $STATUS_SERVER_URL/status.php)
 
-if [ "$res" == "IDLE" ]; then
+if [ "$status" == "IDLE" ]; then
   echo "we can start mongo"
   # start mongo export
   # curl -X POST -d 'MONGO_EXPORT:STARTED' localhost:12345/status.php
   # cd 
   # nohup ./europeana-qa-spark/run-mongo-to-json.sh > run-mongo-to-json.log
   # curl -X POST -d 'MONGO_EXPORT:FINISHED' localhost:12345/status.php
-elif [ "$res" == "MONGO_EXPORT:FINISHED" ]; then
+elif [ "$status" == "MONGO_EXPORT:FINISHED" ]; then
   echo "we can start mongo upload"
 else
-  echo $res
+  echo $status
 fi
 
