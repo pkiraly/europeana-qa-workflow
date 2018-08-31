4157ba41d1aa61f0533516a4da6052d73f2824ae
Peter Kiraly <peter.kiraly@gwdg.de>
2018-08-31 17:19:35 +0200

    Adding management for Mongo export.

--- dispatcher.sh
+++ dispatcher.sh
@@ -10,9 +10,10 @@ if [ "$status" == "IDLE" ]; then
   echo "we can start mongo"
   # start mongo export
   # curl -X POST -d 'MONGO_EXPORT:STARTED' localhost:12345/status.php
-  # cd 
+  # cd ../europeana-qa-spark
   # nohup ./europeana-qa-spark/run-mongo-to-json.sh > run-mongo-to-json.log
   # curl -X POST -d 'MONGO_EXPORT:FINISHED' localhost:12345/status.php
+  # php mongo-hdfs-to-fs-launcher.php >> launch-report.log
 elif [ "$status" == "MONGO_EXPORT:FINISHED" ]; then
   echo "we can start mongo upload"
 else
